2f760aae99cf1e2ac0a61fc16ff091b2a3c60525
yasas <yasas.tharinda@axiatadigitallabs.com>
2025-03-02 12:14:09 +0530

    inital commit

---
+++ src/test/java/com/springLearning/springLearning/SpringLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springLearning.springLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
